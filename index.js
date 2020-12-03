2f6e968c3db9cf4a3d7c7686611631e139bb95e0
matthewxvoorhees <matthew.crnich@gmail.com>
2020-12-03 11:55:37 -0700

    commit tree d78a7d5267e4a5190f60a70bd546a8a0f2e905e4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function createEmployeeRecord(arr){
+    let obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj
+}
+
+function createEmployeeRecords(){
+    
+}
